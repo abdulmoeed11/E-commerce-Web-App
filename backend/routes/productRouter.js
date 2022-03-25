bfa513ed23f847b8bd6dce3256020d917c823ed6
Abdul Moeed <a.moeed11@gmail.com>
2022-03-25 10:18:17 +0300

    added error handlers

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const asynHandler = require("express-async-handler");
-const { async } = require("rxjs");
+// const { async } = require("rxjs");
 
+const mongoose = require("mongoose");
 const Product = require("../models/productModel");
 
 const router = express.Router();
@@ -18,10 +19,12 @@ router.get(
   "/:id",
   asynHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
+    console.log(product);
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Error" });
+      res.status(404);
+      throw new Error("Product not found");
     }
   })
 );
